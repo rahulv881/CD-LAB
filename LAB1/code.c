ee77357d63fce3e29f6019677fce642f4345f8cc
Rahul <rahulv881@gmail.com>
2019-03-12 03:42:01 +0530

    First Commit

---
+++ LAB1/code.c
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+  fostream fout;
+  fout.open("code.c");
+  
+  string s;
+  while(1)
+  {
+    cin>>s;
+    if(s=="exit")             
+    {    
+ 	cout<<"Error: Unable to create File"<<endl;
+        return 0;
+    }
+    
+  }       
+
+ return 0;
+}
