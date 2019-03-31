3b50b1097e9c97f96b30f44cdcd350d21cc722d8
Rahul <rahulv881@gmail.com>
2019-03-31 20:51:05 +0530

    LAB TEST

--- LAB1/test.c
+++
@@ -1,1 +0,0 @@
-#include<iostream>#include<fstream>usingnamespacestd;intmain(){fostreamfout;fout.open("code.c");strings;while(1){cin>>s;if(s=="exit"){cout<<"Error:UnabletocreateFile"<<endl;return0;}}return0;}
